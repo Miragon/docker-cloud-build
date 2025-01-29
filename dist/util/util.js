1892c0380cf5257658f56a3f7d1bd2e22c79ceda
Alexander Praschek <alexander.praschek@miragon.io>
2025-01-29 14:50:49 +0100

    Debug

--- dist/util/util.js
+++ dist/util/util.js
@@ -1,21 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.normalize = exports.delay = void 0;
 /**
  * Delays execution for a specified amount of time.
  *
  * @param ms The time in ms.
  */
-const delay = (ms) => {
+export const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
-exports.delay = delay;
 /**
  * Replaces all characters that are not allowed in image tags with an underscore.
  *
  * @param input The string to normalize
  */
-const normalize = (input) => {
+export const normalize = (input) => {
     return input.replace(/[^A-Za-z0-9._-]/g, "_");
 };
-exports.normalize = normalize;
